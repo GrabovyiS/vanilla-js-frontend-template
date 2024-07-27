74f698251d0e4d23b2d1fbef342387dabab518b5
GrabovyiS <graboviys@yandex.ru>
2024-07-27 20:28:00 +0300

    feat: get special eslint config for tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,26 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import jest from 'eslint-plugin-jest';
+
+const jestConfig = {
+  files: ['**/*.test.js'],
+  ...jest.configs['flat/recommended'],
+};
+
+let jestRules = {
+  ...jestConfig.rules,
+  'no-unused-vars': 'off',
+};
+
+jestConfig.rules = jestRules;
 
 export default [
   js.configs.recommended,
+
+  {
+    ignores: [''],
+  },
 
   {
     languageOptions: {
@@ -16,7 +33,13 @@ export default [
     rules: {
       'no-unused-vars': 'warn',
     },
+
+    plugins: {
+      jest: jest,
+    },
   },
 
+  // Configs cascade, things at the bottom override
+  jestConfig,
   eslintConfigPrettier,
 ];
