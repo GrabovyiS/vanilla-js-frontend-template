406a9a74fe99c89597cc8f1eb672d25433cf8735
GrabovyiS <graboviys@yandex.ru>
2024-08-14 15:15:47 +0300

    feat: add source mapping for both modes

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,4 +3,5 @@ import { merge } from 'webpack-merge';
 
 export default merge(webpackCommon, {
   mode: 'development',
+  devtool: 'inline-source-map',
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,5 @@ import { merge } from 'webpack-merge';
 
 export default merge(webpackCommon, {
   mode: 'production',
+  devtool: 'source-map',
 });
