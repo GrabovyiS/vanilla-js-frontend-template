4d35a71b645215dfa8f126c2a96366eebcbe6934
GrabovyiS <graboviys@yandex.ru>
2024-08-14 16:16:49 +0300

    feat: dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,4 +4,9 @@ import { merge } from 'webpack-merge';
 export default merge(webpackCommon, {
   mode: 'development',
   devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    // By default only watches files added to js bundle
+    watchFiles: ['./src/*.html'],
+  },
 });
